a606372a80620c8ba90e34ae1f5043f2f9356c87
Mateusz Urbański <matek2305@gmail.com>
2015-03-10 19:49:05 +0100

    Added more info to competition data object.

--- app/domain/dto/web/CompetitionData.java
+++ app/domain/dto/web/CompetitionData.java
@@ -12,6 +12,10 @@ public class CompetitionData extends BasicCompetitionInfo {
 
     public String description;
     public String admin;
+    public String leader;
+    public int numberOfPredictors;
+    public int numberOfMatches;
+    public int totalPoints;
     public List<CompetitionTableEntry> table;
 
     public CompetitionData(Competition competition) {
@@ -19,6 +23,10 @@ public class CompetitionData extends BasicCompetitionInfo {
 
         this.description = competition.description;
         this.admin = competition.admin.login;
+        this.numberOfPredictors = competition.predictorsPoints.size();
+        this.numberOfMatches = competition.matches.size();
+        this.totalPoints = competition.predictorsPoints.stream().mapToInt(pp -> pp.points).sum();
         this.table = CompetitionUtils.buildTable(competition.predictorsPoints);
+        this.leader = this.table.stream().filter(e -> e.position == 1).findFirst().get().predictor;
     }
 }
